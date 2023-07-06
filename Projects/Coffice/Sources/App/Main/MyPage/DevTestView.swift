6667dff4f093e9f35ec105fd1727826cd795b955
Applebuddy <eagermin92@gmail.com>
2023-07-06 10:45:58 +0900

    Revert "[Fix] #95 개발자 테스트화면 TextField 바인딩 이벤트 2번씩 발생하는 문제 수정 (#100)"
    This reverts commit 33fb735d34b7ddfc64d07077a698e3d0f3489dd4.

--- Projects/Coffice/Sources/App/Main/MyPage/DevTestView.swift
+++ Projects/Coffice/Sources/App/Main/MyPage/DevTestView.swift
@@ -45,7 +45,6 @@ extension DevTestView {
         "textField with BindingState",
         text: viewStore.binding(\.$textFieldStringWithBindingState)
       )
-      .textFieldStyle(.plain)
       .frame(height: 35)
       .padding(.horizontal, 5)
       .padding(.trailing, 25)
@@ -66,7 +65,6 @@ extension DevTestView {
           send: DevTest.Action.textFieldStringDidChange
         )
       )
-      .textFieldStyle(.plain)
       .frame(height: 35)
       .padding(.horizontal, 5)
       .padding(.trailing, 25)
